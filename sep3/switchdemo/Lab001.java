c6b3a0834fef54584a05e604f22a37b07194bb13
vrushabk29@gmail.com <Vrushab@29>
2025-02-20 23:05:20 +0530

    20 Feb(2)

---
+++ sep3/switchdemo/Lab001.java
@@ -0,0 +1,39 @@
+package sep3.switchdemo;
+
+import java.util.Scanner;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter day : ");
+        int day = sc.nextInt();
+
+        switch (day){
+            case 1:
+                System.out.println("Mon");
+                break;
+            case 2:
+                System.out.println("Tue");
+                break;
+            case 3:
+                System.out.println("Wed");
+                break;
+            case 4:
+                System.out.println("Thur");
+                break;
+            case 5:
+                System.out.println("Fri");
+                break;
+            case 6:
+                System.out.println("Sat");
+                break;
+            case 7:
+                System.out.println("Sun");
+                break;
+            default:
+                System.out.println("Invalid day");
+
+        }
+        System.out.println("Outside the Loop = End ");
+    }
+}
